9ca376d90c3d008daa789d9209a51b2b43d706f0
Juan Montes Sabogal <montessabogaljuan@icloud.com>
2023-04-15 17:10:16 -0500

    Agrega input para buscar los estudiantes por nombre

--- src/componentes/TablaEstudiante.jsx
+++ src/componentes/TablaEstudiante.jsx
@@ -25,26 +25,6 @@ export const TablaEstudiante = ({ listaEstudiantes }) => {
     }
     return (
         <>
-            <table className="table">
-                <thead>
-                    <tr>
-                        <th scope="col">Id Estudiante</th>
-                        <th scope="col">Nombre</th>
-                        <th scope="col">Semestre</th>
-                        <th scope="col">Acciones</th>
-                    </tr>
-                </thead>
-                <tbody>
-                    {
-                        listaEstudiantes.map((estudiante) => <tr key={estudiante.id}>
-                            <td>{estudiante.id}</td>
-                            <td>{estudiante.nombre}</td>
-                            <td>{estudiante.semestre}</td>
-                            <td> <button className="btn btn-info" onClick={editar}>Editar</button></td>
-                        </tr>)
-                    }
-                </tbody>
-            </table>
-        </>
-    )
-}
+      <div className="mb-3">
+        <input type="text" className="form-control" placeholder="Buscar por nombre" onChange={handleChange} value={busqueda} />
+      </div>
