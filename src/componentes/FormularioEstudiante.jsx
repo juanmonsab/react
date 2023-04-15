b20f28bf502d825bf0c59575d24c7a7c2a6cfaf9
Juan Montes Sabogal <montessabogaljuan@icloud.com>
2023-04-15 18:33:20 -0500

    Importa useEffect desde react

--- src/componentes/FormularioEstudiante.jsx
+++ src/componentes/FormularioEstudiante.jsx
@@ -1,4 +1,4 @@
-import { useState } from "react"
+import { useState, useEffect } from "react"
 
 export const FormularioEstudiante = ({ agregar, actualizarEstudiante, estudianteEditar, setEstudianteEditar }) => {
     const [id, setId] = useState("");
