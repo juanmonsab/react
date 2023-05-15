d0d1a88f4bbd4c0faf7317453c6f0843bfd730ce
Juan Montes Sabogal <montessabogaljuan@icloud.com>
2023-05-14 23:23:02 -0500

    Agrega la funcionalidad de filtrar por facultad y limitar el numero de resultados asi como conecta funciones tales como actualizar, eliminar, crear con peticiones http al backend

--- src/componentes/TablaEstudiante.jsx
+++ src/componentes/TablaEstudiante.jsx
@@ -1,33 +1,80 @@
 import React, { useState } from "react";
 import { FormularioEstudiante } from "./FormularioEstudiante";
+import { deleteEstudiantes } from "../peticiones/deleteEstudiantes";
+import { putEstudiantes } from "../peticiones/putEstudiantes";
 
 export const TablaEstudiante = ({ listaEstudiantes, actualizarEstudiante, eliminarEstudiante }) => {
 
-    const [busqueda, setBusqueda] = useState("");
+    const [busquedaNombre, setBusquedaNombre] = useState("");
+    const [busquedaFacultad, setBusquedaFacultad] = useState("");
     const [estudianteEditar, setEstudianteEditar] = useState(null);
+    const [limiteResultados, setLimiteResultados] = useState(0);
 
-    const handleChange = (event) => {
-        setBusqueda(event.target.value);
+    const handleChangeNombre = (event) => {
+        setBusquedaNombre(event.target.value);
+    }
+
+    const handleChangeFacultad = (event) => {
+        setBusquedaFacultad(event.target.value);
+        setLimiteResultados(0);
     }
 
     const handleEditar = (estudiante) => {
         setEstudianteEditar(estudiante);
     }
 
-    const handleEliminar = (id) => {
+    const handleEliminar = async (id) => {
+        await deleteEstudiantes(id);
         eliminarEstudiante(id);
+        alert('El estudiante ha sido eliminado correctamente.');
+    };
+
+    const handleActualizar = async (id, datosEstudiante) => {
+        const data = {
+          nombre: datosEstudiante.nombre,
+          semestre: datosEstudiante.semestre,
+          facultad: datosEstudiante.facultad,
+          programa: datosEstudiante.programa
+        };
+        console.log("Datos del estudiante a actualizar:", data); 
+        await putEstudiantes(id, data);
+        actualizarEstudiante(id, data);
+        alert('El estudiante ha sido actualizado correctamente.');
     }
 
+    const facultades = Array.from(new Set(listaEstudiantes.map((estudiante) => estudiante.facultad)));
+
     const estudiantesFiltrados = listaEstudiantes.filter((estudiante) => {
-        return estudiante.nombre.toLowerCase().includes(busqueda.toLowerCase());
-    });
+        return estudiante.nombre.toLowerCase().includes(busquedaNombre.toLowerCase())
+          && (busquedaFacultad === "" || estudiante.facultad === busquedaFacultad);
+    }).slice(0, limiteResultados || listaEstudiantes.length);
 
     return (
         <>
-            <div className="mb-3">
-                <input type="text" className="form-control" placeholder="Buscar por nombre" onChange={handleChange} value={busqueda} />
+            <div className="row mb-3">
+                <div className="col-md-4">
+                    <input type="text" className="form-control" placeholder="Buscar por nombre" onChange={handleChangeNombre} value={busquedaNombre} />
+                </div>
+                <div className="col-md-4">
+                    <select className="form-select" onChange={handleChangeFacultad} value={busquedaFacultad}>
+                        <option value="">Filtrar por facultad</option>
+                        {facultades.map((facultad) => (
+                            <option key={facultad} value={facultad}>{facultad}</option>
+                        ))}
+                    </select>
+                </div>
+                <div className="col-md-2">
+                    <select className="form-select" onChange={(e) => setLimiteResultados(e.target.value)} value={limiteResultados}>
+                        <option value="">Limite resultados</option>
+                        {[1, 2, 3, 4, 5, 6, 7, 8, 9, 10].map((limite) => (
+                            <option key={limite} value={limite}>{limite}</option>
+                        ))}
+                    </select>
+                </div>
+                <div className="col-md-2">
+                    <button className="btn btn-primary" onClick={() => { setBusquedaFacultad(""); setLimiteResultados(0); }}>Limpiar filtro</button>
+                </div>
             </div>
-
             <table className="table">
                 <thead>
                     <tr>
@@ -35,6 +82,7 @@ export const TablaEstudiante = ({ listaEstudiantes, actualizarEstudiante, elimin
                         <th scope="col">Nombre</th>
                         <th scope="col">Semestre</th>
                         <th scope="col">Facultad</th>
+                        <th scope="col">Programa</th>
                         <th scope="col">Acciones</th>
                     </tr>
                 </thead>
@@ -45,6 +93,7 @@ export const TablaEstudiante = ({ listaEstudiantes, actualizarEstudiante, elimin
                             <td>{estudiante.nombre}</td>
                             <td>{estudiante.semestre}</td>
                             <td>{estudiante.facultad}</td>
+                            <td>{estudiante.programa}</td>  
                             <td>
                                 <button className="btn btn-info mr-2" onClick={() => handleEditar(estudiante)}>
                                     Editar
@@ -65,7 +114,7 @@ export const TablaEstudiante = ({ listaEstudiantes, actualizarEstudiante, elimin
                     </div>
                     <div className="card-body">
                         <FormularioEstudiante
-                            actualizarEstudiante={actualizarEstudiante}
+                            actualizarEstudiante={handleActualizar}
                             estudianteEditar={estudianteEditar}
                             setEstudianteEditar={setEstudianteEditar}
                         />
